bf17a687a4dd0035c649071d2962bfd5bbd506b5
graciry <ngunyigwothaya@gmail.com>
2024-09-23 19:21:32 +0300

    Add

---
+++ Documents/alx-fe-reactjs/github-user-search/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>GitHub User Search</h1>
+        <input type="text" placeholder="Search GitHub users..." />
+      </header>
+    </div>
+  );
+}
+
+export default App;
